35431ac120035ac0025fa2e079bc5614e3bd7bdc
lylova.ekaterina <lylova.ekaterina@rt.ru>
2023-03-21 00:24:36 +0700

    test: solution not exists

--- otus-patterns-homework/src/test/java/otus/patterns/homework/QuadraticTest.java
+++ otus-patterns-homework/src/test/java/otus/patterns/homework/QuadraticTest.java
@@ -1,5 +1,15 @@
 package otus.patterns.homework;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static otus.patterns.homework.Quadratic.solve;
+
 public class QuadraticTest {
+
+    @Test
+    public void solutionNotExists() {
+        assertEquals(0, solve(1, 0, 1).length);
+    }
 
 }
