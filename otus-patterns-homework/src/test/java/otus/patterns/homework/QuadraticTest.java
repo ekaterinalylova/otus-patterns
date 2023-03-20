0b3a4e065ad357bf11b625585c06a707554dc671
lylova.ekaterina <lylova.ekaterina@rt.ru>
2023-03-21 00:49:53 +0700

    test: 2 solutions exist

--- otus-patterns-homework/src/test/java/otus/patterns/homework/QuadraticTest.java
+++ otus-patterns-homework/src/test/java/otus/patterns/homework/QuadraticTest.java
@@ -1,15 +1,29 @@
 package otus.patterns.homework;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static otus.patterns.homework.Quadratic.solve;
 
 public class QuadraticTest {
 
     @Test
     public void solutionNotExists() {
-        assertEquals(0, solve(1, 0, 1).length);
+        double[] actual = solve(1, 0, 1);
+
+        assertEquals(0, actual.length);
+    }
+
+    @Test
+    public void twoSolutionsExists() {
+        double[] actual = solve(1, 0, -1);
+        double[] expected = {1, -1};
+
+        assertThat(List.of(actual), containsInAnyOrder(expected));
     }
 
 }
